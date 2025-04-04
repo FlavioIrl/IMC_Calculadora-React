0563fee8f18fdef44757647d6e91fd19a923cc47
Flavio Irala <flavioiralap@gmail.com>
2025-04-03 20:02:32 -0400

    Projeto finalizado!

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -6,8 +6,25 @@ import styles from './App.module.css'
 import CalculadoraImc from './components/CalculaImc'
 import ResultadoImc from './components/ResultadoImc'
 
+function mudarBorda(imc) {
+  let border = '2px solid var(--cor-padrao)';
+
+  if (imc < 18.5) {
+    border = '2px solid var(--cor-magreza)';
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    border = '2px solid var(--cor-normal)';
+  } else if (imc >= 25.0 && imc <= 29.9) {
+    border = '2px solid var(--cor-sobrepeso)';
+  } else {
+    border = '2px solid var(--cor-obesidade)';
+  }
+
+  return { border };
+}
+
 function App() {
   const [imc, setImc] = useState(null);
+  const { border } = imc !== null ? mudarBorda(imc) : { border: '2px solid var(--cor-padrao)' };
 
   return (
     <>
@@ -15,11 +32,10 @@ function App() {
         <header>
           <h1 className={`text-center fw-bold mt-5 ${styles.title}`}>Calculadora de IMC</h1>
         </header>
-        <div className={`mt-3 shadow-lg ${styles.calcCorpo}`}>
-          <CalculadoraImc setImc={setImc}/>
+        <div className={`mt-3 shadow-lg ${styles.calcCorpo} `} style={{ border }}>
+          <CalculadoraImc setImc={setImc} />
           {
             imc !== null && <ResultadoImc imc={imc} />
-            
           }
         </div >
       </div>
--- src/components/ResultadoImc/index.jsx
+++ src/components/ResultadoImc/index.jsx
@@ -4,44 +4,56 @@ import React from 'react';
 function classificarImc(imc) {
     let categoria;
     let mensagem;
-  
+    let border;
+    let color;
+
     if (imc < 18.5) {
       categoria = "Abaixo do peso";
       mensagem = "Seu IMC está abaixo do recomendado, o que pode indicar um peso abaixo do ideal para a sua saúde. É importante considerar ajustes na alimentação e no estilo de vida.";
+      border = '2px solid var(--cor-magreza)';
+      color = 'var(--cor-magreza)';
+      
     } else if (imc >= 18.5 && imc <= 24.9) {
       categoria = "Peso normal";
       mensagem = "Parabéns! Seu IMC está dentro da faixa saudável, indicando que seu peso está ideal para a sua saúde.";
+      border = '2px solid var(--cor-normal)';
+      color = 'var(--cor-normal)';
+      
     } else if (imc >= 25.0 && imc <= 29.9) {
       categoria = "Sobrepeso";
       mensagem = "Seu IMC está acima do recomendado, o que pode indicar que você tem sobrepeso. Recomendamos avaliar sua alimentação e estilo de vida.";
+      border = '2px solid var(--cor-sobrepeso)';
+      color = 'var(--cor-sobrepeso)';
+
     } else {
       categoria = "Obesidade";
       mensagem = "Seu IMC está na faixa de obesidade. Isso pode aumentar o risco de várias doenças. Consulte um profissional de saúde para orientações personalizadas.";
+      border = '2px solid var(--cor-obesidade)';
+      color = 'var(--cor-obesidade)';
+      
     }
   
-    return { categoria, mensagem };
+    return { categoria, mensagem, border, color };
   }
 
   function ResultadoImc({ imc }) {
-    const { categoria, mensagem } = imc !== null ? classificarImc(imc) : { categoria: '', mensagem: '' };
-
+    const { categoria, mensagem, border, color  } = imc !== null ? classificarImc(imc) : { categoria: '', mensagem: '', border: 'none', color: 'none'};
 
     return (
-        <div className={` shadow-lg mt-5 ${styles.calcCorpo}`}>
+      <div className={` shadow-lg mt-5  ${styles.calcCorpo}` } style={{ border }} >
             <header>
                 <h1 className={`text-center fw-bold  fs-1 ${styles.title}`}>Resultado do seu IMC</h1>
             </header>
             <div className='mt-5'>
                 <div className={`d-flex  ${styles.mostraImc}`}>
-                    <h2 className={`${styles.text}`}>Seu IMC :</h2>
-                    <h2 className={`ps-3 ${styles.text} ${styles.resImc}`}> {imc.toFixed(2)}</h2>
+                    <h2 className={`${styles.text}`}  style={{ color }}>Seu IMC :</h2>
+                    <h2 className={`ps-3 ${styles.text} ${styles.resImc}`} style={{ color }}> {imc.toFixed(2)}</h2>
                 </div>
                 <div className={` ${styles.mostraClasse}`}>
                     <h2 className='text-center mt-4'>Classificação</h2>
                     <hr />
                     <h3 className='fs-5 mt-3 text-center mb-4'>{categoria}</h3>
-                    <p>{mensagem}</p>
-                
+                    <p className={styles.pMensagem}>{mensagem}</p>
                 </div>
             </div>
         </div>
